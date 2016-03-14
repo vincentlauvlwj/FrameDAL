055cb9e4c4328141cf231ba55e502253cdd001f4
Vincent Lau <vincent_lauvl@163.com>
2016-03-14 14:15:48 +0800

    add unit test project

---
+++ FrameDALTests/Core/AppContextTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FrameDAL.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrameDAL.Core.Tests
+{
+    [TestClass()]
+    public class AppContextTests
+    {
+        [TestMethod()]
+        public void OpenSessionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ClearCacheTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTableAttributeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPropertiesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetIdPropertyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetColumnAttributeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetIdAttributeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetManyToOneAttributeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOneToManyAttributeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetManyToManyAttributeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ FrameDALTests/FrameDALTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FrameDAL.Core;
+
+namespace FrameDAL.Tests
+{
+    [TestClass]
+    class FrameDALTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+
+        }
+
+        [TestMethod]
+        public void Select()
+        {
+
+        }
+    }
+}
